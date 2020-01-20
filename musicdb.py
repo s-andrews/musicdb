b23f4068125a086a96c751e61d2ef039a59a5e2b
Simon Andrews <simon@proteo.me.uk>
2020-01-20 12:41:01 +0000

    Added pupils route and basic result

--- musicdb.py
+++ musicdb.py
@@ -6,10 +6,17 @@ APP = Flask(__name__)
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/simon/musicdb.sqlite'
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-from datamodel import School, Pupil
+from datamodel import School, Pupil, PupilInstrument
 
 
 @APP.route('/')
 def index():
     return render_template('base.html')
+
+@APP.route('/pupils')
+def pupils():
+
+    pupils = Pupil.query.all()
+
+    return render_template('pupils.html', pupils=pupils)
 
